37e0a7cedac174847b77fd9460bd1fa31abbd5a5
unknown <gokz22301@gmail.com>
2025-08-12 12:53:52 +0500

    Done javascript topics

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+// // parseInt or parseFloat
 
+// var myString = "44";
+// var myNumber =parseInt(myString)
+// console.log(typeof myString);
+
+
+// var myString = "44.5";
+// var 
+
+// myNumber = parseFloat(myString)
+// console.log(myNumber);
+
+// concatention in javascript\
+
+// var str = "hello  " + "hello";
+// console.log(str);
+
+
+
+
+// ternary operator
+
+// var age = 15;
+// var results = age >= 15? "yes" : "No"
+// console.log(results);
+
+
+// var score = 59;
+// var results =  score >= 60? "pass" : "fail";
+// console.log(results);
+
+// Control statments and loops
+
+
+// var temp = 30;
+// if(temp >=30){
+//     console.log("You can go outside");
+    
+// }else{
+//     console.log("not go outside");
+    
+// }
+
+
+var temp = 5;
+if(temp >=30){
+    console.log("You can go outside");
+    
+}else if(temp >20 && temp > 10){
+    console.log(" YEs also go to outside");
+    
+}
+else{
+    console.log("soo jawo");
+    
+}
