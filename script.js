2d5c489d0e38ffa5311b10511d9f0dd7acba729a
unknown <gokz22301@gmail.com>
2025-08-13 10:30:55 +0500

    Done condition if stmt, slse if stamt and switch stmt

--- script.js
+++ script.js
@@ -43,15 +43,97 @@
 // }
 
 
-var temp = 5;
-if(temp >=30){
-    console.log("You can go outside");
+// var temp = 5;
+// if(temp >=30){
+//     console.log("You can go outside");
     
-}else if(temp >20 && temp > 10){
-    console.log(" YEs also go to outside");
+// }else if(temp >20 && temp > 10){
+//     console.log(" YEs also go to outside");
     
-}
-else{
-    console.log("soo jawo");
+// }
+// else{
+//     console.log("soo jawo");
     
-}
+// }
+
+
+// even Number
+
+// var num = 6;
+// if(num % 2 == 0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+    
+// }
+
+// var num = -12;
+// if (num == 0) {
+//     console.log("number is zero");
+// }else if(num > 0){
+//     console.log("Greater then zero positive");
+    
+// }else{
+//     console.log("Number is negative");
+    
+// }
+
+// switch statments
+
+// var days = "Tuesday";
+// switch (days) {
+//     case "Tuesday":
+//         console.log("Today is Tuesday");
+//         break;
+//     case "wednesday":
+//         console.log("Today is Wednesday");
+//         break;
+//     case "Thursday":
+//         console.log("Today is Thursday");
+//         break;
+//     case "Friday":
+//         console.log("Today is Friday");
+//         break;
+//     case "Tuesday":
+//         console.log("Today is saturday");
+//         break;
+
+//     default:
+//         console.log("No condition match");
+        
+//         break;
+// }
+
+
+
+
+// interview question
+
+// var areaOFShape = "square";
+// var a = 5;
+// var b = 5; 
+// var results;
+
+// switch (areaOFShape) {
+//     case "square":
+//         results = a * a;
+//         console.log(results);
+        
+//         break;
+//     case "rectangle":
+//         results = a * b;
+//         console.log(results);
+        
+//         break;
+//     case "circle":
+//         var r = 2;
+//         results = 3.142 * (r * r);
+//         console.log(results);
+        
+//         break;
+
+//     default:
+//         console.log("no shape match");
+        
+//         break;
+// }
