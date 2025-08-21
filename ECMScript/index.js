9077965c15164b25b57e92f5fb5e02d76ef48a74
unknown <gokz22301@gmail.com>
2025-08-21 12:55:09 +0500

    Done templete string and arrow function

--- ECMScript/index.js
+++ ECMScript/index.js
@@ -38,12 +38,38 @@ console.log("EcmScript");
 
 // example with const
 
-const name = "gulzar";
+// const name = "gulzar";
 
-if(true){
-    const name = "Gulzar khan";
-    console.log(name);
-}
- name = "Aiza";
-console.log(name);
+// if(true){
+//     const name = "Gulzar khan";
+//     console.log(name);
+// }
+//  name = "Aiza";
+// console.log(name);
 
+// Templete literal
+
+// let firstName = "Gulzar";
+// let secondName = "Khan";
+// // let  fullName = firstName + secondName;
+// // templet literal
+// let fullName = `My name is ${firstName}  ${secondName}`
+// console.log(fullName);
+
+
+// let age = 23;
+// let message = `I am ${age} year old`;
+// console.log(message);
+
+// Arrow function =>
+
+    // let sum = (a, b)=>{
+    //     let results = `The sum of ${a} and ${b} is ${a + b}`;
+    //     console.log(results);
+        
+    // }
+    // sum(5, 10);
+
+    let sum = (a, b)=> console.log(`The sum of ${a} and ${b} is ${a + b}`);
+    sum(4, 6)
+    
