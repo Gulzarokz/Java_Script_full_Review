1a7e05ac4ad4e16e34b23768f2ff50e1e000e4d9
unknown <gokz22301@gmail.com>
2025-08-22 15:07:24 +0500

    Done array methods e.g push, pop, unshift, shift

--- Array/index.js
+++ Array/index.js
@@ -27,7 +27,7 @@ console.log("Array of javascript");
 // console.log(fruit);
 
 // travel an array
-let fruits = ["apple", "banana", "Gavava"];
+// let fruits = ["apple", "banana", "Gavava"];
 // // using for of loops
 // for (let item of fruits) {
 //   console.log(`The fruits names are listed ${item}`);
@@ -41,5 +41,66 @@ let fruits = ["apple", "banana", "Gavava"];
 
 // for(let item in fruits){ in is used for index number
 //     console.log(item);
-    
+
 // }
+
+// Array methods
+
+// let myForEachArr = fruits = ["apple", "banana", "Gavava"];
+// fruits.forEach((curElem, index, arr) => {
+//   return  `${index} ${curElem} `;
+//   // console.log(arr);
+// });
+// console.log(myForEachArr);
+
+// map methods
+// let myMaparr =  fruits = ["apple", "banana", "Gavava"];
+// fruits.map((curElem, index, arr) => {
+//    return `${curElem} ${index}`;
+//   console.log(arr);
+// });
+//  console.log(myMaparr);
+
+// let myFavNum =  number = [1, 2, 3, 4, 5];
+
+// number.forEach((curNum) => {
+//   return curNum *2;
+// });
+// console.log(myFavNum);
+
+// let myFavNum =  number = [1, 2, 3, 4, 5];
+// number.map((currNum)=>{
+//     return currNum * 2;
+// });
+// console.log(myFavNum);
+
+// push() methods in an array the push() methods return new lengths
+
+// let fruits = ["apple", "banana", "Gavava"];
+// fruits.push("Grapes");
+// console.log(fruits);
+
+// pop() methods
+// let fruits = ["apple", "banana", "Gavava"];
+// console.log("The pop elements ", fruits.pop());
+// console.log(fruits);
+
+// unshift() methods add elements at the beganing of an array
+//  let fruits = ["apple", "banana", "Gavava"];
+//  fruits.unshift("peach")
+//  console.log(fruits);
+ 
+// shift() methods remove elements at the beganing of an array
+
+//  let fruits = ["apple", "banana", "Gavava"];
+//  console.log(fruits.shift());
+//  console.log(fruits);
+ 
+// splice methods any where you want to add and remove elements in an array
+
+//  let fruits = ["apple", "banana", "Gavava"];
+//  console.log(fruits.splice(1, 1, "grapes", "pear"));
+//  console.log(fruits);
+
+ 
+ 
