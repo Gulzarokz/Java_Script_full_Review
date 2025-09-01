88f0e44b9f7495139cb0555b3826e426a0627d19
unknown <gokz22301@gmail.com>
2025-09-01 10:23:31 +0500

    Done maths objects with some examples

---
+++ Math_obj/index.js
@@ -0,0 +1,30 @@
+console.log("Maths_Objects");
+
+// let piValue = Math.PI;
+// console.log(piValue);
+
+// Math.round()
+// console.log(Math.round(4.3)); simply round up to 4
+// console.log(Math.round(4.7)); it is round up to 5 bcz it is 7 after point
+
+// Math.floor()
+// const floorValue = Math.floor(4.2);
+// console.log(floorValue);
+
+// Math.ceil()
+// const ceilValue = Math.ceil(4.2);
+// console.log(ceilValue);
+
+// Math.pow()
+
+// const mathPower = Math.pow(3, 2);
+// console.log(mathPower);
+
+// ecma 2015
+// console.log(3**2);
+
+
+// random(
+let randomNumber = (Math.random() *100).toFixed(3);
+console.log(randomNumber);
+
